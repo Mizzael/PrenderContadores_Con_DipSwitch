4316f3504d65052d41d36fc2640947d5077f1414
Missael <jonathanmissael_jimenez@ucol.mx>
2019-06-20 11:04:39 -0500

    Comenzando la practica. Comienzo a configurar los puertos

---
+++ SecuenciadeLED.c
@@ -0,0 +1,54 @@
+#include <18F4620.h>
+#fuses HS,NOWDT,NOMCLR
+#use delay(clock=16 MHZ)
+
+//Aqui deben ir las interfaces tales como la rs-232, I^2C
+/*
+#define _DEBUG_SERIAL_
+
+#ifdef _DEBUG_SERIAL_
+   #define TX_232    PIN_C6
+   #define RX_232    PIN_C7
+   #use RS232(BAUD=9600,XMIT=TX_232,BITS=8,PARITY=N,STOP=1,UART1,RCV=RX_232)
+   #use fast_io(c)
+#endif
+*/
+
+//Aqui deben ir la IsR's interrupciones
+   //int_rda
+   //int_timer0
+
+//variables globales
+
+//aqui el void main(void)
+void main(void)
+{
+   set_tris_b(0x00);
+   set_tris_c(0x00);
+   set_tris_d(0x00);
+   
+  int ContadorAnilloLSB=0x01;
+  int ContadorAnilloMSB=0x80;
+ 
+   while(1)
+   {
+      ContadorAnilloLSB=ContadorAnilloLSB>>128;
+      if(ContadorAnilloLSB==0x80)
+      {
+         ContadorAnilloLSB=0x01;
+      }
+      output_b(ContadorAnilloLSB);
+      delay_ms(200);
+      
+      
+      //Aqui usamos mascaras
+      /*if((ContadorAnilloLSB&0x20)==0x20)
+      {
+         output_c(ContadorAnilloLSB);
+      }
+      if((ContadorAnilloLSB&0x20)!=0x20)
+      {
+         output_c(0);
+      }*/
+   }
+}
